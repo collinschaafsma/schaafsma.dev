30c3ca232d8c2b5aa0e3cc0830d2bf43242b6892
Collin Schaafsma <schaafsma@gmail.com>
2024-11-03 12:13:04 -0700

    display name

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Present } from "./components/present"
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
-  );
+    <div className="flex min-h-screen items-center justify-center">
+      <Present duration={1} y={20}>
+        <h1 className="font-sans text-6xl">Collin Schaafsma</h1>
+      </Present>
+    </div>
+  )
 }
